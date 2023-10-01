1eff9ab07c281e82cd4ce074e0ade8e00f6ba4de
Aiyualive <liuaiyu1302@gmail.com>
2023-10-01 18:05:53 +0200

    add prisma and supabase stuff

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,14 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { AppModule } from './app.module';
 import {
     CorsConfig,
     NestConfig,
-    SwaggerConfig,
 } from './configs/config.interface';
+import {TrpcRouter} from "./trpc/trpc.router";
 
 // Boot Strap
 async function bootstrap() {
@@ -31,57 +30,17 @@ async function bootstrap() {
     const configService = app.get(ConfigService);
     const nestConfig = configService.get<NestConfig>('nest');
     const corsConfig = configService.get<CorsConfig>('cors');
-    const swaggerConfig = configService.get<SwaggerConfig>('swagger');
 
     if (!nestConfig) throw new Error('Nest configuration missing');
     if (!corsConfig) throw new Error('CORS configuration missing');
-    if (!swaggerConfig) throw new Error('Swagger configuration missing');
-
-    // Swagger Api
-    if (swaggerConfig.enabled) {
-        const options = new DocumentBuilder()
-            .setTitle(swaggerConfig.title || 'Nestjs')
-            .setDescription(
-                swaggerConfig.description || 'The nestjs API description',
-            )
-            .setVersion(swaggerConfig.version || '1.0')
-            .addBearerAuth(
-                {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
-                    name: 'JWT access authorization',
-                    description: 'Enter JWT access token',
-                    in: 'header',
-                },
-                'JWT-creator',
-            )
-            .addBearerAuth(
-                {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
-                    name: 'JWT access authorization',
-                    description: 'Enter JWT access token',
-                    in: 'header',
-                },
-                'JWT-user',
-            )
-            .build();
-        const document = SwaggerModule.createDocument(app, options);
-
-        SwaggerModule.setup(swaggerConfig.path || 'api', app, document, {
-            swaggerOptions: {
-                persistAuthorization:
-                    swaggerConfig.persistAuthorization || false,
-            },
-        });
-    }
 
     // Cors
     if (corsConfig.enabled) {
         app.enableCors();
     }
+
+    const trpc = app.get(TrpcRouter);
+    trpc.applyMiddleware(app);
 
     await app.listen(process.env.PORT || nestConfig.port || 3005);
 }
