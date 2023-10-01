007829498799cd9265de102a20eaa913d9e6dc22
Aiyualive <liuaiyu1302@gmail.com>
2023-10-01 18:25:54 +0200

    move trpc out

---
+++ apps/frontend/services/trpc.ts
@@ -0,0 +1,8 @@
+
+// export const trpc = createTRPCProxyClient<AppRouter>({
+//     links: [
+//         httpBatchLink({
+//             url: `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`,
+//         }),
+//     ],
+// });
