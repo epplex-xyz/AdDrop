2dc4d214e72202519c46fd98fe23cf4a90b37bee
Aiyualive <liuaiyu1302@gmail.com>
2023-10-06 01:46:21 +0200

    Update CampaignCreationStore.tsx

--- apps/frontend/src/providers/CampaignCreationStore.tsx
+++ apps/frontend/src/providers/CampaignCreationStore.tsx
@@ -1,31 +1,83 @@
 import {create} from 'zustand';
 import { persist } from "zustand/middleware";
 
+interface AdDetailsProps {
+    image: File | null
+    description: string
+    name: string
+    symbol: string
+}
+
+// best to send as Datetime or unixtimestamp
+interface DistributionProps {
+    distributionDate: string
+    minimumUserReach: number
+    userGroups: string[]
+}
+
+
+const defaultAdDetails: AdDetailsProps = {
+    image: null,
+    description: '',
+    name: '',
+    symbol: '',
+};
+
+const defaultDistribution: DistributionProps = {
+    distributionDate: '',
+    minimumUserReach: 0,
+    userGroups: [],
+};
+
+const defaultData: Campaign = {
+    adDetails: defaultAdDetails,
+    distribution: defaultDistribution,
+};
+
 export type Campaign = {
-    username: string,
-    handle: string,
-    avatar: string
-    preferences: string[]
-    mainWalelt: string
+    adDetails: AdDetailsProps,
+    distribution: DistributionProps
+
 }
 
 export type CampaignCreationStore = {
     data: Campaign
-    setData: (UserData) => void
+    setAdDetails: (newAdDetails: AdDetailsProps) => void;
+    setDistribution: (newDistribution: DistributionProps) => void;
+    resetData: () => void;
+
 }
 
 // Zustand with typescript
 // https://stackoverflow.com/questions/69814018/zustand-typescript-persist-how-to-type-store
-const useUserSignupStore = create<CampaignCreationStore>()(
+const useCampaginCreationStore = create<CampaignCreationStore>()(
     persist(
         (set, get) => ({
-            data: {} as Campaign,
-            setData: (data) => {
-                set({ data: data });
+            data: defaultData,
+            setAdDetails: (newAdDetails) => {
+                set((state) => ({
+                    data: {
+                        ...state.data,
+                        adDetails: newAdDetails,
+                    },
+                }));
+            },
+            setDistribution: (newDistribution) => {
+                set((state) => ({
+                    data: {
+                        ...state.data,
+                        distribution: newDistribution,
+                    },
+                }));
+            },
+            resetData: () => {
+                set(() => ({
+                    data: defaultData, // need to add defaults
+                }));
             }
         }),
-        { name: "userSignup" }
+        { name: "campaignCreation" }
     ),
 );
 
-export default useUserSignupStore;
+export default useCampaginCreationStore;
