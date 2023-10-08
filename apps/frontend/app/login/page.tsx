55d040b3eeec3c6296de7da46c4cb3dfa091ef72
Aiyualive <liuaiyu1302@gmail.com>
2023-10-08 13:11:07 +0200

    add login page

---
+++ apps/frontend/app/login/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Screen from "@components/layout/Screen";
+import React from "react";
+import {LoginPage} from "@content/Login/LoginPage";
+
+export default function SignUp() {
+    return (
+        <Screen>
+            <LoginPage/>
+        </Screen>
+    );
+}
