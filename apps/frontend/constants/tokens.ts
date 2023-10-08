75ddd2ae5073672fc7427d478dad58376445fd6f
Aiyualive <liuaiyu1302@gmail.com>
2023-10-08 13:12:32 +0200

    add pubkeys

--- apps/frontend/constants/tokens.ts
+++ apps/frontend/constants/tokens.ts
@@ -1,3 +1,4 @@
+import { PublicKey } from "@solana/web3.js";
 export enum Tokens {
     USDC,
     SOL,
@@ -6,3 +7,10 @@ export enum Tokens {
 }
 
 export const tokenList: string[] = ["USDC", "SOL", "BONK", "EUROe"];
+
+export const tokenKeys: PublicKey[] = [
+    new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
+    new PublicKey("So11111111111111111111111111111111111111112"),
+    new PublicKey("DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"),
+    new PublicKey("2VhjJ9WxaGC3EZFwJG9BDUs9KxKCAjQY4vgd1qxgYWVg")
+];
